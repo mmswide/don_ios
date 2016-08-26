97ff40d6a3588d8d4b77530dfc64a19ab62f70ac
Fantastic <fantastic@fantastics-mac.local>
2016-08-26 19:14:48 +0200

    Initial Commit

---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Fantastic on 26/08/16.
+//  Copyright © 2016 Fantastic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestProject
+//
+//  Created by Fantastic on 26/08/16.
+//  Copyright © 2016 Fantastic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
